4fffccf9614d6ea87f5b64b1fc97f9bab0fa9ba7
Francesca <fracassani@gmail.com>
2020-11-30 17:36:23 +0100

    Sezione "Specialties"

--- js/main.js
+++ js/main.js
@@ -13,5 +13,27 @@ const everlead = new Vue ({
             logo: '-02',
             logoDot: '-01'
         },
+        specialties: [
+            {
+                logo: './svg/svg-0.svg',
+                name: 'Knowing',
+                text: 'lorem ipsum dopor sit amet, quis consect',
+            },
+            {
+                logo: './svg/svg-1.svg',
+                name: 'Selling',
+                text: 'lorem ipsum dopor sit amet, quis consect',
+            },
+            {
+                logo: './svg/svg-2.svg',
+                name: 'Learning',
+                text: 'lorem ipsum dopor sit amet, quis consect',
+            },
+            {
+                logo: './svg/svg-3.svg',
+                name: 'Contact',
+                text: 'lorem ipsum dopor sit amet, quis consect',
+            }
+        ]
     }
 });
