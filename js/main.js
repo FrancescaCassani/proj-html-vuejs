03eb608d562f635ad70d7ff5854f9a64d8eecfcd
Francesca <fracassani@gmail.com>
2020-11-30 11:56:42 +0100

    Impostazione dati JS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const everlead = new Vue ({
+    el: "#everlead",
+    data: {
+        header: [
+            {text: "homes"},
+            {text: "pages"},
+            {text: "blog"},
+            {text: "shop"},
+            {text: "events"},
+            {text: "elements"},
+        ],
+    }
+});
