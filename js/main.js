abe56667ec3fb47ada1faceef535a33e7ac442e3
Francesca <fracassani@gmail.com>
2020-12-01 17:38:49 +0100

    Aggiunta JS ad alcune sezioni

--- js/main.js
+++ js/main.js
@@ -33,6 +33,61 @@ const everlead = new Vue ({
                 name: 'Contact',
                 text: 'lorem ipsum dopor sit amet, quis consect',
             }
+        ],
+        events: [
+            {
+                day: 07,
+                year: 'Jan, 2022',
+                coaching: 'Melbourne Coaching',
+                time: '9:00 am - 5:00 pm, Jan 7, 2022',
+                location: 'Cambridge, MA 02138, USA',
+                moreInfo: 'Read more'
+            },
+            {
+                day: 11,
+                year: 'Jan, 2022',
+                coaching: 'Melbourne Coaching',
+                time: '9:00 am - 5:00 pm, Jan 11, 2022',
+                location: 'Cambridge, MA 02138, USA',
+                moreInfo: 'Read more'
+            },
+            {
+                day: 21,
+                year: 'Jan, 2022',
+                coaching: 'Melbourne Coaching',
+                time: '9:00 am - 5:00 pm, Jan 21, 2022',
+                location: 'Cambridge, MA 02138, USA',
+                moreInfo: 'Read more'
+            }
+        ],
+        news: [
+            {
+                // logo: './img/h1-blog-img-02.jpg',
+                // logo1: './img/h1-blog-img-03.jpg',
+                day: 'May 5, 2019',
+                name: 'Amanda Doe',
+                title: 'Next Investment',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quo quidem obcaecati totam iste eligendi ....',
+                moreInfo: 'Read more'
+            },
+            {
+                // logo: '/img/h1-blog-img-03.jpg',
+                // logo1: './img/h1-img-01.jpg',
+                day: 'May 5, 2019',
+                name: 'Amanda Doe',
+                title: 'Next Investment',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quo quidem obcaecati totam iste eligendi ....',
+                moreInfo: 'Read more'
+            },
+            {
+                // logo: './img/h1-img-01.jpg',
+                // logo1: './img/h1-blog-img-01.jpg"',
+                day: 'May 5, 2019',
+                name: 'Amanda Doe',
+                title: 'Next Investment',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quo quidem obcaecati totam iste eligendi ....',
+                moreInfo: 'Read more'
+            }
         ]
     }
 });
